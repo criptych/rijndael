92d175e0a15964564945991096cea3153fd63b55
criptych <criptych@gmail.com>
2016-03-08 02:03:17 -0500

    Implement block-length CFB

--- rijndael.c
+++ rijndael.c
@@ -392,7 +392,34 @@ size_t rijndael_encrypt_cfb8(rijndael_state *state, const void *plaintext, void 
 }
 
 size_t rijndael_encrypt_cfb(rijndael_state *state, const void *plaintext, void *ciphertext, size_t size) {
-    return 0;
+    uint8_t *indata, *outdata;
+    size_t i, j;
+
+    indata = (uint8_t*)plaintext;
+    outdata = (uint8_t*)ciphertext;
+
+    uint32_t block[8];
+
+    for (i = 0; i < size; i += 4 * state->block_size) {
+
+        rijndael_encrypt_block(state, state->iv);
+
+        for (j = 0; j < state->block_size; ++j) {
+            block[j]  = indata[i + j * 4 + 0] <<  0;
+            block[j] |= indata[i + j * 4 + 1] <<  8;
+            block[j] |= indata[i + j * 4 + 2] << 16;
+            block[j] |= indata[i + j * 4 + 3] << 24;
+
+            state->iv[j] = block[j] ^= state->iv[j];
+
+            outdata[i + j * 4 + 0] = block[j] >>  0;
+            outdata[i + j * 4 + 1] = block[j] >>  8;
+            outdata[i + j * 4 + 2] = block[j] >> 16;
+            outdata[i + j * 4 + 3] = block[j] >> 24;
+        }
+    }
+
+    return i;
 }
 
 size_t rijndael_decrypt_ecb(rijndael_state *state, const void *ciphertext, void *plaintext, size_t size) {
@@ -501,7 +528,36 @@ size_t rijndael_decrypt_cfb8(rijndael_state *state, const void *plaintext, void 
 }
 
 size_t rijndael_decrypt_cfb(rijndael_state *state, const void *plaintext, void *ciphertext, size_t size) {
-    return 0;
+    uint8_t *indata, *outdata;
+    size_t i, j;
+
+    indata = (uint8_t*)plaintext;
+    outdata = (uint8_t*)ciphertext;
+
+    uint32_t block[8];
+
+    for (i = 0; i < size; i += 4 * state->block_size) {
+
+        rijndael_encrypt_block(state, state->iv);
+
+        for (j = 0; j < state->block_size; ++j) {
+            block[j]  = indata[i + j * 4 + 0] <<  0;
+            block[j] |= indata[i + j * 4 + 1] <<  8;
+            block[j] |= indata[i + j * 4 + 2] << 16;
+            block[j] |= indata[i + j * 4 + 3] << 24;
+
+            block[j] ^= state->iv[j];
+
+            outdata[i + j * 4 + 0] = block[j] >>  0;
+            outdata[i + j * 4 + 1] = block[j] >>  8;
+            outdata[i + j * 4 + 2] = block[j] >> 16;
+            outdata[i + j * 4 + 3] = block[j] >> 24;
+
+            state->iv[j] ^= block[j];
+        }
+    }
+
+    return i;
 }
 
 /* wrappers for above specifically for AES usage */
